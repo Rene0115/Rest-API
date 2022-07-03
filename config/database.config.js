b190dec9b307977d3287dcbe50b376c8e73f390c
Rene0115 <enere0115@gmail.com>
2022-07-03 18:13:28 +0100

    database connection

--- config/database.config.js
+++ config/database.config.js
@@ -1,1 +1,13 @@
 import mongoose from 'mongoose';
+import { logger } from '../app.js';
+
+export default () => {
+  mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      logger.info('Connected to the database...');
+    })
+    .catch((error) => {
+      logger.info('Error connectring to database');
+      logger.error(error);
+    });
+};
